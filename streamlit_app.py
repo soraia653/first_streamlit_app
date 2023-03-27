f17067d008d25596e331ac02b97623d34977d9b8
Soraia Tavares <76966637+soraia653@users.noreply.github.com>
2023-03-27 13:44:01 +0200

    Update streamlit_app.py
    set column Fruit as index

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # read CSV file from S3 bucket and display as a df table
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # create pick list so that user can pick the fruits they want to include
 streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
